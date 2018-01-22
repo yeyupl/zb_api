6e16feddc64c52e1f849626e675b6a1e502d5280
罗会铸 <yeyupl@qq.com>
2018-01-22 16:03:28 +0800

    改进策略

--- zb.php
+++ zb.php
@@ -26,12 +26,15 @@ $sleepTime = 10; //每次循环秒杀
 $times = 0; //空操作次数
 $maxTimes = 30;  //最大空操作次数
 $cancelBuyTimes = 0;  //买单撤单次数
+$cancelSellTimes = 0; //卖单撤单次数
 
 $orderMaxAmount = 1000; //每次下单金额
 
 $maxOrder = 2; //最大挂单数
 $buyOrder = 0;  //委买单数
 $sellOrder = 0; //委卖单数
+
+$priceHistory = [];  //历史价格
 
 while (true) {
     //可用数额
@@ -42,6 +45,11 @@ while (true) {
     $ticker = $zbApi->ticker($currency);
     //现价
     $price = $ticker['ticker']['last'];
+
+    $priceHistory[] = $price;
+
+    //均价
+    $avgPrice = array_sum($priceHistory) / count($priceHistory);
 
     $targetMaxAmount = round($orderMaxAmount / $price);
 
@@ -59,15 +67,19 @@ while (true) {
                         $cancelOrder++;
                         if ($order['type'] == 1) {
                             $cancelBuyTimes++;
+                        } else {
+                            $cancelSellTimes++;
                         }
                     }
                 }
-                $times = 0;
                 showLog('超时撤单：' . $cancelOrder);
             } else {
                 showLog('没有委单');
             }
-            sleep(5);
+            $sellOrder = 0;
+            $buyOrder = 0;
+            $times = 0;
+            sleep(3);
         } else {
             $times++;
             showLog('无操作');
@@ -82,8 +94,14 @@ while (true) {
     if ($standardAmount > $orderMaxAmount && $buyOrder < $maxOrder) {
         // 如果还有qc 挂买单 现价折价1% 或者第5档加0.0001 取最大的
 
-        //一直买不到 可能是单行情 为防止一直不成交 挂单档次加上撤单次数因子
-        $bid = max(0, 4 - $cancelBuyTimes);
+        //一直买不到 可能是单边行情 为防止一直不成交 挂单档次加上撤单次数因子
+        if ($price >= $avgPrice) {
+            //上涨行情 加速买入
+            $bid = max(0, 3 - $cancelBuyTimes);
+        } else {
+            //下跌
+            $bid = max(0, 4 - $cancelBuyTimes);
+        }
         $buyPrice = max(round($price * 0.99, 3), $depth['bids'][$bid][0] + 0.0001);
         $buyAmount = floor($orderMaxAmount / $buyPrice);
 
@@ -99,7 +117,16 @@ while (true) {
     }
     if ($targetAmount > $targetMaxAmount && $sellOrder < $maxOrder) {
         // 如果还有bts 挂卖单 现价溢价1% 或者 第5档减少0.0001 取最小
-        $sellPrice = min(round($price * 1.01, 3), $depth['asks'][4][0] - 0.0001);
+
+        //一直卖不出 可能是单边行情 为防止一直不成交 挂单档次加上撤单次数因子
+        if ($price >= $avgPrice) {
+            $ask = max(0, 4 - $cancelSellTimes);
+        } else {
+            //下跌行情 加速卖出
+            $ask = max(0, 3 - $cancelSellTimes);
+        }
+
+        $sellPrice = min(round($price * 1.01, 3), $depth['asks'][$ask][0] - 0.0001);
         $sellAmount = min($targetAmount, $targetMaxAmount);
 
         $result = $zbApi->order($currency, $sellPrice, $sellAmount, 0);
