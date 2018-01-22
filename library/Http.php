17fc93f1edb48f746ea9e87db3e2f236a4278ba8
罗会铸 <yeyupl@qq.com>
2018-01-22 16:41:29 +0800

    up

---
+++ library/Http.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * http请求封装
+ * @author 夜雨 yeyupl@qq.com
+ */
+
+namespace ZB;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\FileCookieJar;
+
+class Http {
+
+    public $cookieFile;
+    public $client;
+    protected $cookieJar;
+    private $options = [
+        'headers' => [
+            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+            'Accept' => 'application/json, text/plain, */*',
+            'Accept-Encoding' => 'gzip, deflate, br',
+            'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,ja;q=0.2',
+            'Connection' => 'keep-alive'
+        ],
+        'timeout' => 10,
+        'verify' => false
+    ];
+
+    /**
+     * http constructor.
+     * http constructor.
+     * @param $cookieFile
+     * @param array $options
+     */
+    public function __construct($cookieFile = '', $options = []) {
+        if ($cookieFile) {
+            $this->cookieFile = $cookieFile;
+            $this->cookieJar = new FileCookieJar($cookieFile, true);
+            $this->client = new Client(['cookies' => $this->cookieJar]);
+        } else {
+            $this->client = new Client();
+        }
+        $this->setOptions($options);
+    }
+
+    /**
+     * set options
+     * @param array $options
+     */
+    public function setOptions(array $options) {
+        if (is_array($options) && $options) {
+            $this->options = $this->array_join_merge($this->options, $options);
+        }
+    }
+
+    /**
+     * GET请求
+     * @param $url
+     * @param array $options
+     * @param bool $decode
+     * @return array|mixed|\stdClass|string
+     */
+    public function get($url, array $options = [], $decode = false) {
+        $content = $this->request($url, 'GET', $options);
+        return $decode ? json_decode($content, true) : $content;
+    }
+
+    /**
+     * POST请求
+     * @param $url
+     * @param array $query
+     * @param bool $decode
+     * @return array|mixed|\stdClass|string
+     */
+    public function post($url, $query = [], $decode = false) {
+        $key = is_array($query) ? 'form_params' : 'body';
+
+        $content = $this->request($url, 'POST', [$key => $query]);
+
+        return $decode ? json_decode($content, true) : $content;
+    }
+
+    /**
+     * POST JSON请求
+     * @param $url
+     * @param array $params
+     * @param bool $decode
+     * @param array $extra
+     * @return array|mixed|\stdClass|string
+     */
+    public function json($url, $params = [], $decode = false, $extra = []) {
+
+        $params = array_merge(['json' => $params], $extra);
+
+        $content = $this->request($url, 'POST', $params);
+
+        return $decode ? json_decode($content, true) : $content;
+    }
+
+
+    /**
+     * HTTP请求
+     * @param $url
+     * @param string $method
+     * @param array $options
+     * @param bool $retry
+     * @return string
+     */
+    public function request($url, $method = 'GET', $options = [], $retry = false) {
+        try {
+            $options = $this->array_join_merge($this->options, $options);
+
+            $response = $this->client->request($method, $url, $options);
+
+            $this->cookieJar && $this->cookieJar->save($this->cookieFile);
+
+            return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            if (!$retry) {
+                return $this->request($url, $method, $options, true);
+            }
+            return false;
+        }
+    }
+
+    /**
+     * 合并多维数组
+     * @param $arr1
+     * @param $arr2
+     * @return array
+     */
+    private function array_join_merge($arr1, $arr2) {
+        if (is_array($arr1) && is_array($arr2)) {
+            $array = [];
+            $keys = array_merge(array_keys($arr1), array_keys($arr2));
+            foreach ($keys as $key) {
+                $arr1[$key] = (isset($arr1[$key]) ? $arr1[$key] : '');
+                $arr2[$key] = (isset($arr2[$key]) ? $arr2[$key] : '');
+                $array[$key] = $this->array_join_merge($arr1[$key], $arr2[$key]);
+            }
+            return $array;
+        } else {
+            return $arr2 ? $arr2 : $arr1;
+        }
+    }
+}
---
+++ library/ZBApi.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * zb.com API接口封装
+ * @author: 夜雨 yeyupl@qq.com
+ */
+
+namespace ZB;
+
+class ZBApi {
+
+    private $accessKey = '';
+    private $secretKey = '';
+    private $hqUrl = 'http://api.zb.com/data/v1/';
+    private $tradeUrl = 'https://trade.zb.com/api/';
+
+    private $http;
+
+    public function __construct($accessKey, $secretKey) {
+        $this->accessKey = $accessKey;
+        $this->secretKey = $secretKey;
+        $this->http = new Http();
+    }
+
+    /** 参数签名
+     * @param array $params
+     * @return string
+     */
+    public function createSign($params = []) {
+        ksort($params);
+        $preSign = http_build_query($params, '', '&');
+        $sign = hash_hmac('md5', $preSign, sha1($this->secretKey));
+        $params['sign'] = $sign;
+        $params['reqTime'] = time() * 1000;
+        return http_build_query($params, '', '&');
+    }
+
+    /**
+     * 发起行情请求
+     * @param $method
+     * @param array $params
+     * @return array|mixed|string
+     */
+    public function hqRequest($method, $params = []) {
+        $url = $this->hqUrl . $method . '?' . http_build_query($params);
+        return $this->http->get($url, [], true);
+    }
+
+    /**
+     * 发起交易请求
+     * @param $method
+     * @param array $params
+     * @return array|mixed|string
+     */
+    public function tradeRequest($method, $params = []) {
+        $params['accesskey'] = $this->accessKey;
+        $params['method'] = $method;
+        $params = $this->createSign($params);
+        $url = $this->tradeUrl . $method . '?' . $params;
+        return $this->http->get($url, [], true);
+    }
+
+    /**
+     * 市场配置
+     * @return array|mixed|string
+     */
+    public function markets() {
+        return $this->hqRequest('markets', []);
+    }
+
+
+    /**
+     * 行情
+     * @param string $market
+     * @return array|mixed|string
+     */
+    public function ticker($market = 'btc_usdt') {
+        return $this->hqRequest('ticker', ['market' => $market]);
+    }
+
+
+    /**
+     * 市场深度
+     * @param string $market
+     * @param int $size
+     * @return array|mixed|string
+     */
+    public function depth($market = 'btc_usdt', $size = 20) {
+        return $this->hqRequest('depth', ['market' => $market, 'size' => $size]);
+    }
+
+
+    /**
+     * 历史成交
+     * @param string $market
+     * @return array|mixed|string
+     */
+    public function trades($market = 'btc_usdt') {
+        return $this->hqRequest('trades', ['market' => $market]);
+    }
+
+
+    /**
+     * K线
+     * @param string $market
+     * @return array|mixed|string
+     */
+    public function kline($market = 'btc_usdt') {
+        return $this->hqRequest('kline', ['market' => $market]);
+    }
+
+
+    /**
+     * 委托下单
+     * @param $currency
+     * @param $price
+     * @param $amount
+     * @param int $tradeType
+     * @return array|mixed|string
+     */
+    public function order($currency, $price, $amount, $tradeType = 1) {
+        return $this->tradeRequest('order', ['currency' => $currency, 'price' => $price, 'amount' => $amount, 'tradeType' => $tradeType]);
+    }
+
+
+    /**
+     * 取消委托
+     * @param $currency
+     * @param $id
+     * @return array|mixed|string
+     */
+    public function cancelOrder($currency, $id) {
+        return $this->tradeRequest('cancelOrder', ['currency' => $currency, 'id' => $id]);
+    }
+
+
+    /**
+     * 获取委托单
+     * @param $currency
+     * @param $id
+     * @return mixed
+     */
+    public function getOrder($currency, $id) {
+        return $this->tradeRequest('getOrder', ['currency' => $currency, 'id' => $id]);
+    }
+
+    /**
+     * 获取多个委托买单或卖单，每次请求返回10条记录
+     * @param $currency
+     * @param int $pageIndex
+     * @param int $tradeType
+     * @return array|mixed|string
+     */
+    public function getOrders($currency, $pageIndex = 1, $tradeType = 1) {
+        return $this->tradeRequest('getOrders', ['currency' => $currency, 'pageIndex' => $pageIndex, 'tradeType' => $tradeType]);
+    }
+
+    /**
+     * 获取多个委托买单或卖单，每次请求返回pageSize<100条记录
+     * @param $currency
+     * @param int $pageIndex
+     * @param int $pageSize
+     * @param int $tradeType
+     * @return array|mixed|string
+     */
+    public function getOrdersNew($currency, $pageIndex = 1, $pageSize = 1, $tradeType = 1) {
+        return $this->tradeRequest('getOrdersNew', ['currency' => $currency, 'pageIndex' => $pageIndex, 'pageSize' => $pageSize, 'tradeType' => $tradeType]);
+    }
+
+
+    /**
+     * 获取未成交或部份成交的买单和卖单，每次请求返回pageSize<=10条记录
+     * @param $currency
+     * @param int $pageIndex
+     * @param int $pageSize
+     * @return array|mixed|string
+     */
+    public function getUnfinishedOrdersIgnoreTradeType($currency, $pageIndex = 1, $pageSize = 10) {
+        return $this->tradeRequest('getUnfinishedOrdersIgnoreTradeType', ['currency' => $currency, 'pageIndex' => $pageIndex, 'pageSize' => $pageSize]);
+
+    }
+
+    /**
+     * 获取用户信息
+     * @return array|mixed|string
+     */
+    public function getAccountInfo() {
+        return $this->tradeRequest('getAccountInfo');
+    }
+
+
+    /**
+     * 获取币可用余额
+     * @param $coin
+     * @return int
+     */
+    public function getAvailableAmount($coin) {
+        $coin = strtoupper($coin);
+        $result = $this->getAccountInfo();
+        if (!isset($result['code'])) {
+            foreach ($result['result']['coins'] as $v) {
+                if ($v['enName'] == $coin) {
+                    return $v['available'];
+                }
+            }
+        }
+        return 0;
+    }
+
+}
+
